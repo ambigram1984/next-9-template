81967e753ab9c6b4f5790c23c84344a40c412e04
Tyler Freeman <tyler.freeman@willowtreeapps.com>
2020-08-20 22:07:10 -0400

    WIP: Storybook broked

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,24 @@
+import { addDecorator, configure } from "@storybook/react"
+import * as React from "react"
+import { GlobalStyles } from "../styles/global-styles"
+
+const req = require.context("../", true, /.stories.tsx$/)
+
+const withGlobal = (cb) => (
+  <>
+    <GlobalStyles />
+    {cb()}
+  </>
+)
+
+function loadStories() {
+  req.keys().forEach((filename) => req(filename))
+}
+
+addDecorator(withGlobal)
+
+configure(loadStories, module)
+
 module.exports = {
   addons: ["@storybook/addon-knobs/register"],
 }
