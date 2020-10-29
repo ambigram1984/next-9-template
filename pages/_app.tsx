84d193cadb0b844f1b3a86c965d9b116280d16d5
Tyler Freeman <tyler.freeman@willowtreeapps.com>
2020-10-29 13:01:49 -0400

    Refactor to use function instead of class

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,19 @@
 import App from "next/app"
+import { AppProps } from "next/dist/next-server/lib/router/router"
 import dynamic from "next/dynamic"
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
-    const ReactAxeWrapper = dynamic<unknown>(
-      () =>
-        import("../utils/react-axe-wrapper").then((mod) => mod.ReactAxeWrapper),
-      {
-        ssr: false,
-      }
-    )
-    return (
-      <>
-        <ReactAxeWrapper />
-        <Component {...pageProps} />
-      </>
-    )
-  }
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const ReactAxeWrapper = dynamic<unknown>(
+    () =>
+      import("../utils/react-axe-wrapper").then((mod) => mod.ReactAxeWrapper),
+    {
+      ssr: false,
+    }
+  )
+  return (
+    <>
+      <ReactAxeWrapper />
+      <Component {...pageProps} />
+    </>
+  )
 }
-
-export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,11 +11,6 @@ import Document, {
 } from "next/document"
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
   render() {
     return (
       <Html lang="en">
